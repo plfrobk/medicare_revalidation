03258eed872d94d273decc68b1a7a951bfd1dc60
Rob Kairis <rjkairis@gmail.com>
2024-01-31 12:04:17 -0600

    Add more python packages and fix relative path issue

--- helpers/install_chromedriver.py
+++ helpers/install_chromedriver.py
@@ -34,30 +34,30 @@ class InstallChromeDriver():
     latestChromeDriverVersion = get(f'https://googlechromelabs.github.io/chrome-for-testing/LATEST_RELEASE_{currentVersionRoot}').text
     
     if self.system == 'Darwin':
-      localChromeDriverDirectory = './bin/chrome_driver'
+      localChromeDriverDirectory = '/bin/chrome_driver/'
     
       try:
-        remove(localChromeDriverDirectory + '/chrome-mac-arm64/')
+        remove(localChromeDriverDirectory + 'chrome-mac-arm64/')
       except FileNotFoundError:
         print('Previous ChromeDriver already removed!')
 
       download(f'https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/{latestChromeDriverVersion}/mac-arm64/chrome-mac-arm64.zip', localChromeDriverDirectory)
       unpack_archive(filename=localChromeDriverDirectory + '/chrome-mac-arm64.zip', extract_dir=localChromeDriverDirectory, format='zip')
-      remove(localChromeDriverDirectory + '/chrome-mac-arm64.zip')
+      remove(localChromeDriverDirectory + 'chrome-mac-arm64.zip')
       
       print("Successfully installed latest version of Chrome Driver")
 
     elif self.system == 'Windows':
-      localChromeDriverDirectory = '.\\bin\\chrome_driver'
+      localChromeDriverDirectory = '\\bin\\chrome_driver\\'
 
       try:
-        remove(localChromeDriverDirectory + "\\chrome-win64\\")
+        remove(localChromeDriverDirectory + "chrome-win64\\")
       except FileNotFoundError:
         print('Previous ChromeDriver already removed!')
       
       download(f'https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/{latestChromeDriverVersion}/win64/chrome-win64.zip', localChromeDriverDirectory)
       unpack_archive(filename=localChromeDriverDirectory + '\\chrome-win64.zip', extract_dir=localChromeDriverDirectory, format='zip')
-      remove(localChromeDriverDirectory + '\\chrome-win64.zip')
+      remove(localChromeDriverDirectory + 'chrome-win64.zip')
       
       print("Successfully installed latest version of Chrome Driver")
 
